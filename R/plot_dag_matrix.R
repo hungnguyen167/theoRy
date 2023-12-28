e1d4ca10588c9190ea96b1b53e19b4746b0ad0b0
nbreznau <breznau.nate@gmail.com>
2023-12-28 19:55:14 +0100

    fixed plot_dag_matrix to allow user defined titles
    updated build_formula_matrix to adapt to the user adding their own models to the causal_matrix, this needs to be improved significantly, but it gets the job done for now for our working paper
    
    saved working paper models 1-6 as png

--- R/plot_dag_matrix.R
+++ R/plot_dag_matrix.R
@@ -8,15 +8,17 @@ plot_dag_matrix <- function(dag_matrix,
                             choose_plots_mas = "all", # must be either all or "{x}", where 'x' is the MAS
                             formula_matrix_name = formula_matrix, # in case using a different named formula matrix
                             save_plots = FALSE, # option to save plots as png
-                            choose_save_plots = NA, # if not NA, must be a vector with plot numbers
                             title = TRUE,
                             #suppress_plots = FALSE, # to view plots or not while they are plotting
-                            title_pos = 0.5) {
+                            title_pos = 0.5,
+                            title_labels = formula_matrix_name$model,
+                            plot_xlim = c(minX,1.1), 
+                            plot_ylim = c(-1.1,1.5)) {
     
     
     
     # error if choose_plots not a numeric vector or all
-    if(choose_plots == "all") {
+    if(!is.vector(choose_plots)) {
         # start with all plots specified for selection
         plots <- as.vector(formula_matrix_name$model) 
         message("Plotting all model numbers")
@@ -25,7 +27,7 @@ plot_dag_matrix <- function(dag_matrix,
         plots <- as.numeric(formula_matrix_name$model[formula_matrix_name$model %in% choose_plots])
         if(is.vector(choose_plots) & is.numeric(choose_plots)) {
             # NOTE 1: need to find a way to add commas between choose_plots numbers
-            message(paste(c("Plotting only plots ", choose_plots," to png files")))
+            message(paste(c("Plotting only plots ", choose_plots)))
         }
         else {
             stop("choose_plots must be a numeric vector with model numbers")
@@ -34,8 +36,8 @@ plot_dag_matrix <- function(dag_matrix,
     
     # check choose_plots_mas
     # right now we can only search for a single MAS, update to vector in future for multiple
-    if(choose_plots_mas == "all") {
-        message("Plotting all MAS")
+    if(is.vector(choose_plots_mas)) {
+        message("Not subsetting by MAS")
         # choose all
         plots_mas <- as.vector(formula_matrix_name$mas) 
     } else {
@@ -49,13 +51,13 @@ plot_dag_matrix <- function(dag_matrix,
         }
     }
     
-    if(save_plots & sum(choose_save_plots, na.rm = T) > 0) {
-        if(is.vector(choose_save_plots) & is.numeric(choose_save_plots)) {
+    if(save_plots) {
+        if(is.vector(choose_plots) & is.numeric(choose_plots)) {
             # NOTE 1: need to find a way to add commas between choose_plots numbers
-            message(paste(c("Saving only plots ", choose_save_plots," to png files")))
+            message(paste(c("Saving only plots ", choose_plots," to png files")))
         }
         else {
-            stop("choose_save_plots must be a numeric vector with model numbers")
+            stop("choose_plots must be a numeric vector with model numbers")
         }        
     }
     
@@ -68,16 +70,18 @@ plot_dag_matrix <- function(dag_matrix,
     # find lowest X position in dagitty object
     minX <- as.numeric(readRDS(here::here("Results", "minX.RDS"))[1,1])-0.1
     
-    if(title) {
+    formula_matrix_name$title <- title_labels
+    
+    if(title) { 
         for (l in formula_matrix_name$model){
-            plot(dag_matrix[[l]], xlim = c(minX,1.1), ylim = c(-1.1,1.5))
+            plot(dag_matrix[[l]], xlim = plot_xlim, ylim = plot_ylim)
             text(0.5,-1, paste0("MAS = ",formula_matrix_name$mas[formula_matrix_name$model == l]))
-            text(minX+title_pos,1.4, paste0("Model ",formula_matrix_name$model[l]), font = 2)
+            text(minX+title_pos,1.4, paste0("Model ", formula_matrix_name$title[formula_matrix_name$model == l]), font = 2)
             dag_plots <- append(dag_plots, list(recordPlot()))
             }
     } else {
         for (l in formula_matrix_name$model){
-            plot(dag_matrix[[l]], xlim = c(minX,1.1), ylim = c(-1.1,1.5))
+            plot(dag_matrix[[l]], xlim = plot_xlim, ylim = plot_ylim)
             text(0.5,-1, paste0("MAS = ",formula_matrix_name$mas[formula_matrix_name$model == l]))
             dag_plots <- append(dag_plots, list(recordPlot()))
         }
@@ -86,15 +90,8 @@ plot_dag_matrix <- function(dag_matrix,
         
     
     if(save_plots) {
-        if(sum(choose_save_plots, na.rm =T) == 0) {
-            lengn = formula_matrix_name$model
-        } else {
-            lengn = choose_save_plots
-        }
-        
-  
-        for (n in 1:length(lengn)) {
-            png(file = here::here("Results", paste0("model_", formula_matrix_name$model[n], ".png")), width = 480, height = 240)
+        for (n in 1:length(formula_matrix_name$model)) {
+            png(file = here::here("Results", paste0("model_", title_labels[n], ".png")), width = 480, height = 240)
             replayPlot(dag_plots[[n]])
             dev.off()
         }

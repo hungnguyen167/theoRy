9ebbb01014a432f626565b0e901032ea24e1d1b5
nbreznau <breznau.nate@gmail.com>
2024-01-02 11:37:22 +0100

    Removed run_theory.R, replaced with theoRy.R
    Merged changes from both parties into one 02_Working_Paper

--- R/theoRy.R
+++ R/theoRy.R
@@ -1,5 +1,23 @@
+message("theoRy v1.0")
+message("Breznau, Nate and Hung H.V. Nguyen")
+
+packages <- c("tidyverse",
+              "dagitty",
+              "here",
+              "data.table",
+              "QCA")
+
+pacman::p_load(packages, character.only = T)
+
 source("R/build_causal_matrix.R")
+
 source("R/build_formula_matrix.R")
+
+source("R/build_dag_matrix.R")
+
+source("R/identify_compatible.R")
+
+source("R/plot_dag_matrix.R")
 
 run_theory <- function(inputs){
     causal_matrix <- build_causal_matrix(inputs, return_node=FALSE)

83c778b0519cbe76234005005cfddb4e34975c64
Hung Nguyen <vhnh1607@gmail.com>
2024-01-07 15:45:27 +0100

    Update build_set_matrix.R
    - Fixed a problem where {{outcome_var}} does not return column. Changed to !!sym(outcome_var).
    - Simplify code, use pivot_wider instead of loop

--- R/build_set_matrix.R
+++ R/build_set_matrix.R
@@ -1,33 +1,32 @@
 # build a matrix ready for set theory analysis out of the causal matrix
-
+require(dyplyr)
 build_set_matrix <- function(causal_matrix,
-                             outcome_var = "test_compatible",
+                             outcome_var="test_compatible",
                              outcome_positive = "compatible", # choose the string that indicates a positive outcome
-                             cmp_matrix_name = cmp_matrix) {
+                             cmp_matrix) {
     
     causal_matrix_t <- causal_matrix %>%
         as.data.frame() %>%
-        subset(select = c(model,component))
+        subset(select = c(model,component)) %>% 
+        pivot_wider(
+            names_from = component, values_from = component, 
+            values_fn = list(component = function(x) 1), 
+            values_fill = list(component = 0)
+        )
     
-    for (v in causal_matrix_t$component) {
-        causal_matrix_t[,paste0(v)] <- ifelse(causal_matrix_t$component == v, 1, 0)
-    }
-    
-    # get each component as a variable
-    # this leaves single variables at 0, this may be enough info
-    causal_matrix_t <- causal_matrix_t %>%
-        subset(select = -component) %>%
-        group_by(model) %>%
-        summarise_all(max, na.rm = T)
+
     
     # add formula results
-    formula_matrix_t <- as.data.frame(cmp_matrix_name) %>% dplyr::select(model, {{outcome_var}})
+    formula_matrix_t <- as.data.frame(cmp_matrix) %>% dplyr::select(model, !!sym(outcome_var))
    
-    ref_mod = cmp_matrix_name[test_compatible == "reference model", c(model, formula, mas)]
-    
+    ref_mod <- cmp_matrix %>%
+        as.data.frame() %>%
+        dplyr::filter(!!sym(outcome_var) == "reference model") %>%
+        dplyr::select(model, formula, mas)
     formula_matrix_t <- subset(as.data.frame(formula_matrix_t), model != as.numeric(ref_mod[1,1])) %>%
-        mutate(outcome = ifelse({{outcome_var}} == outcome_positive, 1, 0)) %>%
-        dplyr::select(-{{outcome_var}})
+        mutate(outcome = ifelse(!!sym(outcome_var) == outcome_positive, 1, 0)) %>%
+        dplyr::select(-!!sym(outcome_var))
+    
     
     causal_matrix_t <- causal_matrix_t %>%
         subset(model != as.numeric(ref_mod[1,1])) %>%
@@ -38,7 +37,7 @@ build_set_matrix <- function(causal_matrix,
     print(paste("Reference Model MAS = ", ref_mod[1,3], sep = ""))
     print(paste("Set Matrix OUTCOME set to ", outcome_var, " = ", outcome_positive))
     return(as.data.frame(causal_matrix_t))
-    
+
 }
 
 message("function build_set_matrix loaded")
